5c345a42dbc0dff60a3ed0f66283967613f18ffa
vieekk <vieekk@gmail.com>
2013-05-04 19:53:14 +0200

    Basic functionality.

--- src/jsloth.js
+++ src/jsloth.js
@@ -1,11 +1,21 @@
-var Jsloth = (function ($) {
+var Jsloth = (function () {
 	var Jsloth = function () {
-		$('body').prepend('<p>Jsloth is running.</p>');
+		var sloth = document.createElement('div');
+		var attr = document.createAttribute('class');
+		attr.value = 'sloth';
+		sloth.setAttributeNode(attr);
+		this.sloth = document.getElementsByTagName('body')[0].appendChild(sloth);
+	};
+
+	var exec = function () {
+
 	};
 
 	Jsloth.prototype.constructor = Jsloth;
+	Jsloth.prototype.exec = exec;
 	
 	return Jsloth;
-})(jQuery);
+})();
 
-new Jsloth();
+var sloth = new Jsloth();
+sloth.exec();

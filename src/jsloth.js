4755fffc7d4054b8f1596aab2f1fbed7f29ac695
vieekk <vieekk@gmail.com>
2013-05-04 20:15:06 +0200

    JS wall found...

--- src/jsloth.js
+++ src/jsloth.js
@@ -5,15 +5,26 @@ var Jsloth = (function () {
 		attr.value = 'sloth';
 		sloth.setAttributeNode(attr);
 		this.sloth = document.getElementsByTagName('body')[0].appendChild(sloth);
+		this.x = 0;
+		this.y = 0;
 	};
 
 	var exec = function () {
+		// setInterval(update, 1000/10);
+		update();
+	};
 
+	var update = function () {
+		this.x++;
+		this.y++;
+		console.dir(this.x);
+		// this.sloth.style.left = this.x + 'px';
+		// this.sloth.style.bottom = this.y + 'px';
 	};
 
 	Jsloth.prototype.constructor = Jsloth;
 	Jsloth.prototype.exec = exec;
-	
+
 	return Jsloth;
 })();
 

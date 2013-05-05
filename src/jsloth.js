18e95a2ab0330f82aba7d9d8799954bd4340bf79
vieekk <vieekk@gmail.com>
2013-05-05 16:11:05 +0200

    It's alive!

--- src/jsloth.js
+++ src/jsloth.js
@@ -1,24 +1,45 @@
 var Jsloth = (function ($) {
 	function Jsloth(options) {
 		var defaults = {
-			fps: 30,
+			fps: 60,
+			position: 'top left',
 			size: 64
 		};
 		this.settings = $.extend({}, defaults, options);
 		this.create();
-		this.direction = 0;
-		this.x = this.y = -this.settings.size;
 	}
 
 	function create() {
-		this.$sloth = $('<div class="sloth"></div>').prependTo('body');
-		var px = '-' + this.settings.size + 'px';
-		this.$sloth
-			.addClass('sloth-' + this.settings.size)
+		this.x = this.y = -this.settings.size;
+		this.direction = 0;
+		var position = this.settings.position.split(' ');
+
+		for (var i = 0; i < position.length; i++) {
+			switch (position[i]) {
+				case 'left':
+					this.x = -this.settings.size;
+					break;
+				case 'right':
+					this.x = $(window).width();
+					break;
+				case 'top':
+					this.y = -this.settings.size;
+					break;
+				case 'bottom':
+					this.y = $(window).height();
+					break;
+			}
+		}
+
+		this.$sloth = $('<div class="sloth"></div>')
+			.prependTo('body')
 			.css({
-				left: px,
-				top: px
+				width: this.settings.size,
+				height: this.settings.size,
+				left: this.x,
+				top: this.y
 			});
+		this.wayfinder();
 	}
 
 	function exec() {
@@ -29,18 +50,26 @@ var Jsloth = (function ($) {
 	}
 
 	function logic() {
-		var width = $(window).width();
-		var height = $(window).height();
-		// logic...
+		if (this.random(1000) === 0) {
+			this.direction = this.random(4);
+			return;
+		}
+
+		this.wayfinder();
 	}
 
 	function move() {
 		var rand = function () {
-			return Math.floor((Math.random()*5) + 1)
+			return random.call(this, 5, 1);
 		};
-		
-		this.x += rand();
-		this.y += rand();
+
+		this.x = this.direction % 2 === 0 ? this.x + rand() : this.x - rand();
+		this.y = this.direction < 2 ? this.y + rand() : this.y - rand();
+	}
+
+	function random(max, start) {
+		if (typeof start === 'undefined') start = 0;
+		return Math.floor((Math.random()*max) + start);
 	}
 
 	function update() {
@@ -53,12 +82,40 @@ var Jsloth = (function ($) {
 			});
 	}
 
+	function wayfinder() {
+		var width = $(window).width();
+		var height = $(window).height();
+		var x = this.direction % 2 === 0;
+		var y = this.direction < 2;
+		
+		if (this.x >= width)
+			x = false;
+		else if (this.x + this.settings.size < 0)
+			x = true;
+
+		if (this.y >= height)
+			y = false;
+		else if (this.y + this.settings.size < 0)
+			y = true;
+
+		if (x && y)
+			this.direction = 0;
+		else if (!x && y)
+			this.direction = 1;
+		else if (x && !y)
+			this.direction = 2;
+		else
+			this.direction = 3;
+	}
+
 	Jsloth.prototype.constructor = Jsloth;
 	Jsloth.prototype.create = create;
 	Jsloth.prototype.exec = exec;
 	Jsloth.prototype.logic = logic;
 	Jsloth.prototype.move = move;
+	Jsloth.prototype.random = random;
 	Jsloth.prototype.update = update;
+	Jsloth.prototype.wayfinder = wayfinder;
 
 	return Jsloth;
 })(jQuery);

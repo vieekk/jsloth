1c51969bb463b07531653cfda111ba1457875776
vieekk <vieekk@gmail.com>
2013-05-05 11:37:19 +0200

    Why use anonymous functions?

--- src/jsloth.js
+++ src/jsloth.js
@@ -1,29 +1,41 @@
 var Jsloth = (function () {
-	var Jsloth = function () {
+	function Jsloth() {
+		this.create();
+	}
+
+	function create() {
 		var sloth = document.createElement('div');
 		var attr = document.createAttribute('class');
 		attr.value = 'sloth';
 		sloth.setAttributeNode(attr);
 		this.sloth = document.getElementsByTagName('body')[0].appendChild(sloth);
+	}
+
+	function exec() {
 		this.x = 0;
 		this.y = 0;
-	};
+		var that = this;
+		setInterval(function () {
+			that.update();
+		}, 1000/10);
+	}
 
-	var exec = function () {
-		// setInterval(update, 1000/10);
-		update();
-	};
+	function move() {
 
-	var update = function () {
+	}
+
+	function update() {
 		this.x++;
 		this.y++;
-		console.dir(this.x);
-		// this.sloth.style.left = this.x + 'px';
-		// this.sloth.style.bottom = this.y + 'px';
-	};
+		this.sloth.style.left = this.x + 'px';
+		this.sloth.style.bottom = this.y + 'px';
+	}
 
 	Jsloth.prototype.constructor = Jsloth;
+	Jsloth.prototype.create = create;
 	Jsloth.prototype.exec = exec;
+	Jsloth.prototype.move = move;
+	Jsloth.prototype.update = update;
 
 	return Jsloth;
 })();
